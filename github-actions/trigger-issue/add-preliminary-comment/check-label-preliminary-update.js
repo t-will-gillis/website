d18335eb30e50e9eaa99c10d8af8309f224a5bc5
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2023-12-29 11:00:11 -0800

    Update check-label-preliminary-update.js

--- github-actions/trigger-issue/add-preliminary-comment/check-label-preliminary-update.js
+++ github-actions/trigger-issue/add-preliminary-comment/check-label-preliminary-update.js
@@ -41,29 +41,39 @@ function obtainLabels(){
  * @returns - A boolean which tells whether we are supposed to post a preliminary update based on the given issue checks
  */
 
+// function postComment(existingLabels){
+//     //issue states that we are to post the comment if--> there is a role: back end/devOps tag...(continued on next comment)
+//     if(existingLabels.includes("role: back end/devOps")){
+//         return true
+//     }
+
+//     // or if there is a role: front end tag
+//     else if(existingLabels.includes("role: front end")){
+//         return true
+//     }
+
+//     //or if there is a role: design tag
+//     else if(existingLabels.includes("role: design")){
+//         return true
+//     }
+
+//     //or if there is a role: user research
+//     else if(existingLabels.includes("role: user research")){
+//         return true
+//     }
+
+//     //otherwise we return a false
+//     return false
+// }
+
+// Simplify postComment()
 function postComment(existingLabels){
-    //issue states that we are to post the comment if--> there is a role: back end/devOps tag...(continued on next comment)
-    if(existingLabels.includes("role: back end/devOps")){
-        return true
-    }
+    // Post the comment if there is a role: back end/devOps tag...(continued on next comment)
+    const labelsThatTriggerComments = ["role: back end/devOps", "role: front end", "role: design", "role: user research"]
+    console.log('in add-preliminary-comment.js')
+    console.log(existingLabels.some(value => labelsThatTriggerComments.includes(value)));
+    return existingLabels.some(value => labelsThatTriggerComments.includes(value));
 
-    // or if there is a role: front end tag
-    else if(existingLabels.includes("role: front end")){
-        return true
-    }
-
-    //or if there is a role: design tag
-    else if(existingLabels.includes("role: design")){
-        return true
-    }
-
-    //or if there is a role: user research
-    else if(existingLabels.includes("role: user research")){
-        return true
-    }
-
-    //otherwise we return a false
-    return false
 }
 
 module.exports = main
