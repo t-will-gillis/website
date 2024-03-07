28cfe5cc559e14cf3a6920b443ffb150103d2461
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-03-07 13:55:14 -0800

    Update label_update_name.js

--- github-actions/utils/label_update_name.js
+++ github-actions/utils/label_update_name.js
@@ -11,17 +11,17 @@ async function main({ g, c }) {
   // const labelNameNew = context.payload.label.name;
   const labelId = context.payload.label.id;
   const labelName = context.payload.name;
+
+  console.log('-----------------------------------------------------------------------');
+  console.log('Context and current info for edited label: ');
   console.log(context.payload.label);
   console.log('-----------------------------------------------------------------------');
+  console.log('What was changed: ');
   console.log(context.payload.changes);
   console.log('-----------------------------------------------------------------------');
   
   if(context.payload.changes.name) {
 
-    const prevName1 = context.payload.changes.name.from;
-    console.log('Name changed!!!! before');
-    console.log(prevName1);
-    
     // Retrieve label directory
     const filepath = 'github-actions/utils/_data/label_directory.json';
     const rawData = fs.readFileSync(filepath, 'utf8');
@@ -30,10 +30,12 @@ async function main({ g, c }) {
     const prevName = context.payload.changes.name.from;
     console.log('Name changed!!!!');
     console.log(prevName);
+    console.log(labelName);
     
     for(let [key, value] of Object.entries(data)) {
       if (value.includes(prevName)) {
         console.log('Changing label name value from: ' + prevName + ' to: ' + labelName);
+        break;
       } else {
         console.log('Something went wrong!');
       }
