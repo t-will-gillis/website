ba4e6e401b031b0f7efdf9f8ff4039912ae7b177
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-03-06 18:24:31 -0800

    Update return-label-name.js

--- github-actions/utils/return-label-name.js
+++ github-actions/utils/return-label-name.js
@@ -1,1 +1,13 @@
+// Global variables
+var github;
+var context;
 
+async function main({ g, c }, artifactContent) {
+  github = g;
+  context = c;
+
+  // Retrieve lists data from json file written in previous step
+  let parsedEvent = JSON.parse(artifactContent);
+
+  console.log('**********************************************');
+  console.log(parsedEvent);
