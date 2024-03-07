4a0809a4affbef0b2b7d9bfa6a3e3364c41519c2
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-03-07 09:59:15 -0800

    Update return-label-name.js

--- github-actions/utils/return-label-name.js
+++ github-actions/utils/return-label-name.js
@@ -6,19 +6,20 @@ async function main({ g, c }) {
   github = g;
   context = c;
 
-  const labelNameNew = context.payload.label.name;
+  // const labelNameNew = context.payload.label.name;
   const labelId = context.payload.label.id
 
   console.log('****************************************************');
-  console.log(context);
+  console.log(context.payload.label);
+  console.log('label id ********************************************');
+  console.log(context.payload.label.id);
+  console.log('label name ******************************************');
+  console.log(context.payload.label.name);
+  console.log('payload changes *************************************');
+  console.log(context.payload.changes);
   console.log('****************************************************');
-  console.log('****************************************************');
-  console.log('1 ' + context.payload.label);
-  console.log('2 ' + context.payload.label.id);
-  console.log('3 ' + context.payload.label.name + '\n');
-  console.log('4 ' + context.payload.changes);
   if(context.payload.changes.name){
-    console.log('5 ' + context.payload.changes.name);
+    console.log(context.payload.changes.name);
   } else {
     console.log('    did not change name');
   } 
