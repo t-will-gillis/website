dafce9679d9389f9e9e384731e5ceaf2f49801c1
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-03-07 09:52:35 -0800

    Update return-label-name.js

--- github-actions/utils/return-label-name.js
+++ github-actions/utils/return-label-name.js
@@ -13,20 +13,18 @@ async function main({ g, c }) {
   console.log(context);
   console.log('****************************************************');
   console.log('****************************************************');
-  console.log('1' + context.payload.label);
-  console.log('2' + context.payload.label.id);
-  console.log('3' + context.payload.label.name);
-  console.log('4' + context.payload.event.changes);
-  console.log('5' + context.payload.changes);
-  console.log('****************************************************');
+  console.log('1 ' + context.payload.label);
+  console.log('2 ' + context.payload.label.id);
+  console.log('3 ' + context.payload.label.name + '\n');
+  console.log('4 ' + context.payload.changes);
+  if(context.payload.changes.name){
+    console.log('5 ' + context.payload.changes.name);
+  } else {
+    console.log('    did not change name');
+  } 
 
 
-  
-  // Retrieve lists data from json file written in previous step
-  // let parsedEvent = JSON.parse(artifactContent);
 
-  // console.log('**********************************************');
-  // console.log(parsedEvent);
 
 
 
