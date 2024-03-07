0958a9cce945130896978dfa286a277e761b7b13
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-03-07 09:46:32 -0800

    Update return-label-name.js

--- github-actions/utils/return-label-name.js
+++ github-actions/utils/return-label-name.js
@@ -6,13 +6,18 @@ async function main({ g, c }) {
   github = g;
   context = c;
 
+  const labelNameNew = context.payload.label.name;
+  const labelId = context.payload.label.id
+
   console.log('****************************************************');
   console.log(context);
   console.log('****************************************************');
   console.log('****************************************************');
-  console.log(context.payload.label);
-  console.log(context.payload.changes.color);
-  console.log(context.payload.label.id);
+  console.log('1' + context.payload.label);
+  console.log('2' + context.payload.label.id);
+  console.log('3' + context.payload.label.name);
+  console.log('4' + context.payload.event.changes);
+  console.log('5' + context.payload.changes);
   console.log('****************************************************');
 
 
