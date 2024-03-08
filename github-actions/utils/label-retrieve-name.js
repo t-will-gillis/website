455abd4b8d1651c090bd11d30b40e55fbd54fbce
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-03-07 21:50:56 -0800

    Update label-retrieve-name.js

--- github-actions/utils/label-retrieve-name.js
+++ github-actions/utils/label-retrieve-name.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 async function labelRetrieveName(...keyNames) {
 
   // Retrieve label directory
-  const filepath = './_data/label-directory.json';
+  const filepath = 'github-actions/utils/_data/label-directory.json';
   const rawData = fs.readFileSync(filepath, 'utf8');
   const data = JSON.parse(rawData);
 
