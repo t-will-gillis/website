c12239eda8cffb7b42c8f4e61d399d8554eaf152
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-03-07 22:02:49 -0800

    Update add-label.js

--- github-actions/trigger-schedule/add-update-label-weekly/add-label.js
+++ github-actions/trigger-schedule/add-update-label-weekly/add-label.js
@@ -8,10 +8,9 @@ const labelRetrieveName = require('../../utils/label-retrieve-name');
 var github;
 var context;
 
-const statusUpdatedLabel = 'statusUpdated';
-const toUpdateLabel = 'toUpdate';
-const inactiveLabel = '2WeeksInactive';
-[statusUpdatedLabel, toUpdateLabel, inactiveLabel] = labelRetrieveName(statusUpdatedLabel, toUpdateLabel, inactiveLabel);
+const statusUpdatedLabel = labelRetrieveName('statusUpdated');
+const toUpdateLabel = labelRetrieveName('toUpdate');
+const inactiveLabel = labelRetrieveName('2WeeksInactive');
 console.log(statusUpdatedLabel, toUpdateLabel, inactiveLabel);
 
 const updatedByDays = 3; // If there is an update within 3 days, the issue is considered updated
