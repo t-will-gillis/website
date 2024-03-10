11cf659ec2e4be122cf118cf3e474431efaa49d8
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-03-09 20:29:38 -0800

    Update label-retrieve-name.js

--- github-actions/utils/label-retrieve-name.js
+++ github-actions/utils/label-retrieve-name.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 
 /*
  * Match label reference name(s) to the label display name(s) from the /_data/label_directory.json
- * @param {Array} keyNames       - List of reference names to look up display names
- * @return {Array} displayNames  - List of display names
+ * @param {String} keyName       - Reference name to look up display name
+ * @return {String} displayName  - Display name
  */
-async function labelRetrieveName(...keyNames) {
+async function labelRetrieveName(keyName) {
 
   // Retrieve label directory
   const filepath = 'github-actions/utils/_data/label-directory.json';
   const rawData = fs.readFileSync(filepath, 'utf8');
   const data = JSON.parse(rawData);
 
-  const displayNames = keyNames.map(keyName => data[keyName][1]);
-  return displayNames;
+  const displayName = data[keyName][1]);
+  return displayName;
 }
 
 module.exports = labelRetrieveName;
