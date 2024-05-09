452b4e7d7318ff2d8f9b22a6af2e69609cfff2b0
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-05-09 15:00:21 -0700

    Update add-team-member.js

--- github-actions/utils/add-team-member.js
+++ github-actions/utils/add-team-member.js
@@ -8,7 +8,7 @@
 async function addTeamMember(github, context, team, username){
 
   const baseMember = await github.request('GET /orgs/{org}/teams/{team_slug}/memberships/{username}', {
-      org: context.repo.owner,
+      org: 'hackforla',
       team_slug: team,
       username: username,
     });
