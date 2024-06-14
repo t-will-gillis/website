d2fee585ea3ffc1e646cc5688cc119fdb915a703
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-06-14 10:51:57 -0700

    Update graphql-query.js
    initial setup for modularized instructions

--- github-actions/utils/graphql-query.js
+++ github-actions/utils/graphql-query.js
@@ -1,37 +1,45 @@
+// Ref: https://developer.ibm.com/articles/awb-consuming-graphql-apis-from-plain-javascript/
+
+// Import modules
+const fetch = require('node-fetch');
+
+/*
+ * Function that makes a GraphQL *query* to the GitHub API
+ * @param{ Object } data          - body of the data query pre-formed prior to funct. call       
+ * @param{ String } token         - GitHub token
+ * @param( String } message       - optional user agent message
+ *
+ */
 
 
+function graphQlQuery(data, token, message = '' ) {                   // remove message if not needed?
 
-async function graphQLquery(githubToken, userAgentMessage) {
-  const mutation = JSON.stringify({
-    query: `mutation HideOutdatedComment($nodeid: ID!){ 
-        minimizeComment(input:{
-          classifier:OUTDATED,
-          subjectId: $nodeid
-        }) {
-          clientMutationId
-          minimizedComment {
-            isMinimized
-            minimizedReason
-          }
-        }
-      }`,
-    variables: {
-      nodeid: node_id
+  const DATA = data;
+  const GH_TOKEN = token;
+  const USER_AGENT_MESSAGE = message;
+
+  const response = await fetch(
+    'https://api.github.com/graphql',
+    {
+      method: 'POST',
+      body: DATA,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'bearer ' + GH_TOKEN,
+        'user-agent': USER_AGENT_MESSAGE,                                   //    << needed? 
+      },
     }
-  });
+  );
 
-  const options = {
-    hostname: 'api.github.com',
-    path: '/graphql',
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'bearer ' + githubToken,
-      'user-agent': userAgentMessage
-    }
-  };
+  const json = await response.json();
+  return json;
 
-  // copied from https://developer.ibm.com/articles/awb-consuming-graphql-apis-from-plain-javascript/
+  
+}
+                       
+  
+
+ /* 
   const req = https.request(options, (res) => {
     let data = '';
     // console.log(`statusCode: ${res}`);
@@ -51,3 +59,6 @@ async function graphQLquery(githubToken, userAgentMessage) {
   req.write(mutation);
   req.end();
 }
+*/
+
+module.exports = graphQlQuery;
