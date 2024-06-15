25df17d4a123dbf4d4e21e925dc91ae9377f9b08
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-06-15 13:17:27 -0700

    Create grpahql-check-status.js

---
+++ .github/workflows/grpahql-check-status.js
@@ -0,0 +1,46 @@
+name: GraphQL Query 3
+on: 
+  workflow_dispatch:
+
+jobs:
+  Get-Nodes:
+    runs-on: ubuntu-latest
+    steps:
+
+      - name: Get project data
+        env:
+          GH_TOKEN: ${{ secrets.GRAPHQL }}
+          USER: "t-will-gillis"
+          PROJECT_NUMBER: 5
+
+        run: |
+          gh api graphql -f query='
+            query {
+              repository(owner:"t-will-gillis", name:"website") {
+                pullRequest(number:782){
+                  reviews(last:5){
+                   nodes{
+                    state,
+                    url, author{login},
+                    createdAt,
+                   }
+                 }
+                  commits(last:1){
+                    nodes{
+                      commit{
+                        status{
+                          state
+                          contexts{
+                            state,
+                            context,
+                            description,
+                            createdAt,
+                            targetUrl
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }'
