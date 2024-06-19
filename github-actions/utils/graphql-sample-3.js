8556fb8dae1e17beb2756ae52ccd5eec6a2e01bd
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-06-19 10:45:42 -0700

    Create graphql-sample-3.js

---
+++ github-actions/utils/graphql-sample-3.js
@@ -0,0 +1,40 @@
+// Example query to return last three issue titles in repo
+
+// Global variables
+var github
+var context
+
+async function main({ g, c }) {
+  github = g
+  context = c
+
+
+  const query = `
+    query lastIssues($owner: String!, $repo: String!, $num: Int!) {
+      repository (owner: $owner, name: $repo) {
+        issues(last: $num) {
+          edges {
+            node {
+              title
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  let raw_data = await github.graphql(query, {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    num: 10,
+    });
+  let edges = raw_data.repository.issues.edges;
+
+  for (let i = 0; i < edges.length; i++) {
+    result = edges[i].node.title;
+    console.log(`Issue title: ${result}`);
+  }
+
+}
+
+module.exports = main;
