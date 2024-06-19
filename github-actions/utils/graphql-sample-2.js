333dc5ba5625ef5e2e099039405f4b4aca4ea94d
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-06-19 09:35:53 -0700

    Update graphql-sample-2.js

--- github-actions/utils/graphql-sample-2.js
+++ github-actions/utils/graphql-sample-2.js
@@ -6,7 +6,7 @@ async function main({ g, c }) {
   github = g
   context = c
 
- 
+ /*
   const {
     viewer: { login },
   } = await github.graphql(`
@@ -18,6 +18,27 @@ async function main({ g, c }) {
 
   console.log('test');
   console.log(`viewer = ${viewer}`);
+  */
+  
+  const { lastIssues } = await github.graphql(
+    `
+      query lastIssues($owner: String!, $repo: String!, $num: Int = 3) {
+        repository(owner: $owner, name: $repo) {
+          issues(last: $num) {
+            edges {
+              node {
+                title
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      owner: "t-will-gillis",
+      repo: "website",
+    },
+  );
 }
 
 module.exports = main;
