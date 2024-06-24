29856d5af824adc083b07ec47995a9edf468a558
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-06-24 14:23:01 -0700

    Create graphql-status-6.js

---
+++ github-actions/utils/graphql-status-6.js
@@ -0,0 +1,52 @@
+// Example query to return status of issues
+const fetch = require('node-fetch');
+
+// Global variables
+var github
+var context
+
+async function main({ g, c }) {
+  github = g
+  context = c
+
+
+const { allIssues } = await github.graphql(
+  `
+  query {
+    user(login: "t-will-gillis") {
+      projectV2(number:5) {
+        items(last: 100) {
+          pageInfo {
+            hasNextPage
+          }
+          nodes {
+            statusField: fieldValueByName(name: "Status") {
+              __typename
+              ...statusFieldDetails
+            }
+            content {
+              __typename
+              ... on Issue {
+                ...issueDetails
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  fragment issueDetails on Issue {
+    number
+    title
+    id
+  }
+
+  fragment statusFieldDetails on ProjectV2ItemFieldSingleSelectValue {
+    name
+  }
+  `;
+
+console.log(allIssues);
+
+module.exports = main;
