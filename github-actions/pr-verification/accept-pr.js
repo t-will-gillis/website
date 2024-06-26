9ba10e6098ff2e37e8b757f02282631d098bf681
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-06-26 15:00:35 -0700

    Update accept-pr.js

--- github-actions/pr-verification/accept-pr.js
+++ github-actions/pr-verification/accept-pr.js
@@ -1,9 +1,10 @@
 // Import modules
 const fs = require('fs');
 const checkTeamMembership = require('../utils/check-team-membership');
+const postIssueComment = require('../utils/post-issue-comment');
 
 // Global variables
-var girhub
+var github
 var context
 
 async function main({ g, c }) {
@@ -11,13 +12,26 @@ async function main({ g, c }) {
   context = c;
 
   // Check if PR author is on the 'website-write' team
-  const prAuthor = context.payload.pull_request.head.repo.owner.login;
-  const prAuthor2 = context.payload.sender.login;
+  const prAuthor = context.payload.sender.login;
   const prNumber = context.payload.number;
+  const team = 'website-write';
 
+  console.log(`Owner: ${context.repo.owner`);
+  console.log(`Repo: ${context.repo.repo}`);
   console.log(`PR Author: ${prAuthor}`);
-  console.log(`PR Author2: ${prAuthor2}`);
   console.log(`PR Number: ${prNumber}`);
+
+  if (!checkTeamMembership(github, prAuthor, team) {
+    await github.rest.issues.update({
+        owner : 't-will-gillis',
+        // owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: prNumber,
+        state : 'closed'
+      });
+    let comment = 'You must be a member of the HFLA website team in order to create pull requests. Please see our page on how to join us as a member at HFLA: https://www.hackforla.org/getting-started. If you have been though onboarding, and feel you have received this message in error, please message us in the #hfla-site team Slack channel with the link to this PR.'
+    postIssueComment(prNumber, comment, github, context);
+  }
 }
 
 
