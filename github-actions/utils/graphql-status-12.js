eb061b4bf8b2af55269a92d30c0eb89daa075e9d
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-06-25 21:49:09 -0700

    Update graphql-status-12.js

--- github-actions/utils/graphql-status-12.js
+++ github-actions/utils/graphql-status-12.js
@@ -7,7 +7,68 @@ async function main({ g, c }) {
   github = g
   context = c
 
-   
+  const query = `
+    query findIssuesAndSubfields($owner: String!, $repo: String!, $projectNumber: Int!, $num: Int=25, $cursor: String) {
+      repository(owner: $owner, name: $repo) {
+        projectV2(number: $projectNumber) {
+          items(first: $num, after: $cursor) {
+            nodes{
+              fieldValueByName(name:"Status") {
+                __typename
+                ... on ProjectV2ItemFieldSingleSelectValue {
+                  id
+                  name
+                }
+              }
+              content{
+                __typename
+                ... on Issue {
+                  number
+                  title
+                  id
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    owner = "hackforla",
+    // owner = context.repo.owner,
+    repo = context.repo.repo,
+    projectNumber = 86,
+  };
+    
+  try {
+    const result = await github.graphql.paginate(query, variables);
+
+    // Parse the returned data
+    const items = result.repository.projectV2.items.nodes.map(node => ({
+      issue: {
+        number: node.content.number,
+        title: node.content.title,
+        id: node.content.id,
+      },
+      status: {
+        id: node.fieldValueByName.id,
+        name: node.fieldValueByName.name,
+      }
+    }));
+
+    console.log('Parsed Items:', items);
+
+    // Set output for use in other workflow steps
+    context.setOutput('items', JSON.stringify(items));
+  } catch (error) {
+    console.error('Error executing GraphQL query:', error);
+  }
+
+
+
+  
   const query = `
     query findStatusSubfieldIds($login: String!, $projectNumber: Int!) {
       organization(login: $login) {
