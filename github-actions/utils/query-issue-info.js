ee1d6d8f1583183c87df998fe0bd2f3857f12347
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-19 15:03:22 -0700

    Update query-issue-info.js

--- github-actions/utils/query-issue-info.js
+++ github-actions/utils/query-issue-info.js
@@ -48,7 +48,7 @@ async function queryIssueInfo(github, context) {
     // and find the node that contains the 'name' property, then get its 'name' value
     const statusName = projectItems[0].fieldValues.nodes.find(item => item.hasOwnProperty('name')).name;
 
-    console.log(`Success! For Issue #${ISSUE_NUMBER}, found id = '${id}' and status = '${statusName}'`);
+    // console.log(`Success! For Issue #${ISSUE_NUMBER}, found id = '${id}' and status = '${statusName}'`);
     return { id, statusName };
   } catch(error) {
     throw new Error(`Error finding Issue #${ISSUE_NUMBER} id and status; error = ${error}`);
