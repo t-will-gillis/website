31a6a993744e841ee9fc512161409159bc91d964
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-19 11:22:55 -0700

    Update query-issue-info.js

--- github-actions/utils/query-issue-info.js
+++ github-actions/utils/query-issue-info.js
@@ -1,35 +1,40 @@
 /**
  * @description - Get item info using its issue number
- * @returns {Object} - An object containing the item ID and its status name
+ * @params {Object} context  - Context of the function calling getItemInfo()
+ * @returns {Object}         - An object containing the item ID and its status name
  */
-async function getItemInfo() {
-  try {
-    const query = `query($owner: String!, $repo: String!, $issueNum: Int!) {
-      repository(owner: $owner, name: $repo) {
-        issue(number: $issueNum) {
-          id
-          projectItems(first: 100) {
-            nodes {
-              id
-              fieldValues(first: 100) {
-                nodes {
-                  ... on ProjectV2ItemFieldSingleSelectValue {
-                    name
-                  }
+async function getItemInfo(context) {
+
+  // const { owner, repo } = context.repo;
+  ISSUE_NUMBER = context.payload.issue.number;
+  
+  const query = `query($owner: String!, $repo: String!, $issueNum: Int!) {
+    repository(owner: $owner, name: $repo) {
+      issue(number: $issueNum) {
+        id
+        projectItems(first: 100) {
+          nodes {
+            id
+            fieldValues(first: 100) {
+              nodes {
+                ... on ProjectV2ItemFieldSingleSelectValue {
+                  name
                 }
               }
             }
           }
         }
       }
-    }`;
+    }
+  }`;
 
-    const variables = {
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issueNum: context.payload.issue.number
-    };
+  const variables = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issueNum: ISSUE_NUMBER
+  };
 
+  try { 
     const response = await github.graphql(query, variables);
 
     // Extract the list of project items associated with the issue
@@ -43,9 +48,10 @@ async function getItemInfo() {
     // and find the node that contains the 'name' property, then get its 'name' value
     const statusName = projectItems[0].fieldValues.nodes.find(item => item.hasOwnProperty('name')).name;
 
+    console.log(`Success! For Issue #${ISSUE_NUMBER}, found id = '${id}' and status = '${status}'`);
     return { id, statusName };
   } catch(error) {
-    throw new Error("Error updating item's status: " + error);
+    throw new Error("Error finding Issue #${ISSUE_NUMBER} id and status: " + error);
   }
 }
 
