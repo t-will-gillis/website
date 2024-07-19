6f2e90ed9f70f66cab461a0347184cc6bc9b159f
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-19 14:08:20 -0700

    Update query-issue-info.js

--- github-actions/utils/query-issue-info.js
+++ github-actions/utils/query-issue-info.js
@@ -3,7 +3,7 @@
  * @params {Object} context  - Context of the function calling getItemInfo()
  * @returns {Object}         - An object containing the item ID and its status name
  */
-async function getItemInfo(context) {
+async function queryIssueInfo(context) {
 
   // const { owner, repo } = context.repo;
   ISSUE_NUMBER = context.payload.issue.number;
@@ -48,11 +48,11 @@ async function getItemInfo(context) {
     // and find the node that contains the 'name' property, then get its 'name' value
     const statusName = projectItems[0].fieldValues.nodes.find(item => item.hasOwnProperty('name')).name;
 
-    console.log(`Success! For Issue #${ISSUE_NUMBER}, found id = '${id}' and status = '${status}'`);
+    console.log(`Success! For Issue #${ISSUE_NUMBER}, found id = '${id}' and status = '${statusName}'`);
     return { id, statusName };
   } catch(error) {
-    throw new Error("Error finding Issue #${ISSUE_NUMBER} id and status: " + error);
+    throw new Error(`Error finding Issue #${ISSUE_NUMBER} id and status: ${error}`);
   }
 }
 
-module.exports = getItemInfo;
+module.exports = queryIssueInfo;
