2d96203d5e803513e8d0552686d6f923591295b3
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-19 21:18:52 -0700

    Update query-issue-info.js

--- github-actions/utils/query-issue-info.js
+++ github-actions/utils/query-issue-info.js
@@ -18,6 +18,7 @@ async function queryIssueInfo(issueNum, github, context) {
               nodes {
                 ... on ProjectV2ItemFieldSingleSelectValue {
                   name
+                  id
                 }
               }
             }
