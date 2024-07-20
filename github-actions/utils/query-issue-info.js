f08fa1f12a727ad0729dc9020096df21a2703a9c
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-20 14:34:30 -0700

    Update query-issue-info.js

--- github-actions/utils/query-issue-info.js
+++ github-actions/utils/query-issue-info.js
@@ -19,7 +19,7 @@ async function queryIssueInfo(issueNum, github, context) {
         projectItems(first: 1) {
           nodes {
             id
-            fieldValues(first: 1) {
+            fieldValues(first: 5) {
               nodes {
                 ... on ProjectV2ItemFieldSingleSelectValue {
                   name
@@ -47,7 +47,7 @@ async function queryIssueInfo(issueNum, github, context) {
     // Since there is always one item associated with the issue,
     // directly get the item's ID from the first index
     const id = projectItems[0].id;
-
+    console.log(`id: ${id}`);
     // Iterate through the field values of the first project item
     // and find the node that contains the 'name' property, then get its 'name' value
     const statusName = projectItems[0].fieldValues.nodes.find(item => item.hasOwnProperty('name')).name;
