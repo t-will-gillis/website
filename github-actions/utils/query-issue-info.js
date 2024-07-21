0f204f2254d2a63af82dea3c8ffb5010a6398159
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-20 17:25:47 -0700

    Update query-issue-info.js

--- github-actions/utils/query-issue-info.js
+++ github-actions/utils/query-issue-info.js
@@ -3,14 +3,10 @@
  * @params {Object} context  - Context of the function calling getItemInfo()
  * @returns {Object}         - An object containing the item ID and its status name
  */
-async function queryIssueInfo(issueNum, github, context) {
+async function queryIssueInfo(github, context) {
 
   // const { owner, repo } = context.repo;
-  // let issueNum = context.payload.issue.number;
-  
-  // console.log(context.repo.owner);
-  // console.log(context.repo.repo);
-  // console.log(issueNum);
+  let issueNum = context.payload.issue.number;
   
   const query = `query($owner: String!, $repo: String!, $issueNum: Int!) {
     repository(owner: $owner, name: $repo) {
@@ -41,17 +37,21 @@ async function queryIssueInfo(issueNum, github, context) {
   try { 
     const response = await github.graphql(query, variables);
     console.log(response);
+    
     // Extract the list of project items associated with the issue
     const projectItems = response.repository.issue.projectItems.nodes;
-    console.log(projectItems);
+    console.log(`projectItems: ${projectItems}`);
+    
     // Since there is always one item associated with the issue,
     // directly get the item's ID from the first index
     const id = projectItems[0].id;
     console.log(`id: ${id}`);
+    
     // Iterate through the field values of the first project item
     // and find the node that contains the 'name' property, then get its 'name' value
     const statusName = projectItems[0].fieldValues.nodes.find(item => item.hasOwnProperty('name')).name;
-    console.log(`status ${statusName}`);
+    console.log(`status: ${statusName}`);
+    
     // console.log(`Success! For Issue #${ISSUE_NUMBER}, found id = '${id}' and status = '${statusName}'`);
     return { id, statusName };
   } catch(error) {
