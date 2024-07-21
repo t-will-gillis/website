07cb359aa52eab6ddf23656662c9987a8fd77568
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-20 17:39:39 -0700

    Update query-issue-info.js

--- github-actions/utils/query-issue-info.js
+++ github-actions/utils/query-issue-info.js
@@ -3,10 +3,10 @@
  * @params {Object} context  - Context of the function calling getItemInfo()
  * @returns {Object}         - An object containing the item ID and its status name
  */
-async function queryIssueInfo(github, context) {
+async function queryIssueInfo(github, context, issueNum) {
 
   // const { owner, repo } = context.repo;
-  let issueNum = context.payload.issue.number;
+  // let issueNum = context.payload.issue.number;  DELETE
   
   const query = `query($owner: String!, $repo: String!, $issueNum: Int!) {
     repository(owner: $owner, name: $repo) {
@@ -36,7 +36,7 @@ async function queryIssueInfo(github, context) {
 
   try { 
     const response = await github.graphql(query, variables);
-    console.log(`issueNum: ${issueNum}`);
+    // console.log(`issueNum: ${issueNum}`);
     
     // Extract the list of project items associated with the issue
     const projectItems = response.repository.issue.projectItems.nodes;
@@ -50,7 +50,7 @@ async function queryIssueInfo(github, context) {
     // Iterate through the field values of the first project item
     // and find the node that contains the 'name' property, then get its 'name' value
     const statusName = projectItems[0].fieldValues.nodes.find(item => item.hasOwnProperty('name')).name;
-    console.log(`status: ${statusName}`);
+    // console.log(`status: ${statusName}`);
     
     // console.log(`Success! For Issue #${ISSUE_NUMBER}, found id = '${id}' and status = '${statusName}'`);   DELETE
     return { id, statusName };
