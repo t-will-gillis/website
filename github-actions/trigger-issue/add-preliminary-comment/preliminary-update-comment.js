f02cc25116bca901c3a77824ab88720e44fd604c
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-20 17:36:32 -0700

    Update preliminary-update-comment.js

--- github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
+++ github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
@@ -137,11 +137,11 @@ async function assignedToAnotherIssue() {
       const isPreWork = issue.labels.some(label => label.name === "Complexity: Prework");
 
       // ...or has "Emergent Request" Status
-      console.log('checking ER`);
+      console.log('checking ER');
       const inEmergentRequestStatus = (await queryIssueInfo(github, context)).statusName === Emergent_Requests;
     
       // ...or has "New Issue Approval" Status
-      console.log('checking NIA`);
+      console.log('checking NIA');
       const inNewIssueApprovalStatus = (await queryIssueInfo(github, context)).statusName === New_Issue_Approval;
     
       // Include the assignee's other issue only if none of the conditions are met
