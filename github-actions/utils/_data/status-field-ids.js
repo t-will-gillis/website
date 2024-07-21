8046abc1200cf112f7b717f3525e60fe8861b816
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-20 21:24:37 -0700

    Update status-field-ids.js

--- github-actions/utils/_data/status-field-ids.js
+++ github-actions/utils/_data/status-field-ids.js
@@ -46,27 +46,28 @@ function statusFieldIds(statusField) {
   //   ]);
 
     const statusValues = new Map([          // for TWG
-      [Agendas, "941e9965"],
-      [Ice_Box, "d039008d"],
-      [Emergent_Requests, "8749a929"],    
-      [New_Issue_Approval, "31082c42"],
-      [Prioritized_Backlog, "3d615590"],
-      [ERs_And_Epics, "387a9d00"],
-      [In_Progress, "4be08c01"],
-      [Questions_In_Review, "8889577c"],
-      [QA, "b58f4901"],
-      [UAT, "18b48c1f"],
-      [QA_Senior_Review, "4bf4d791"],
-      [Done, "199de7b8"],
-      [Development_Team, "79b0f439"],
-      [PR_Needs_Review,  "d3dec1c7"],
-      [PR_Pending_Approval, "a113bef6"],
-      [PR_Approved, "21e33c3c"],
-      [Feature_Branch, "024a83ed"],
-      [IGNORE, "d662f195"],
+      ["Agendas", "941e9965"],
+      ["Ice_Box", "d039008d"],
+      ["Emergent_Requests", "8749a929"],    
+      ["New_Issue_Approval", "31082c42"],
+      ["Prioritized_Backlog", "3d615590"],
+      ["ERs_And_Epics", "387a9d00"],
+      ["In_Progress", "4be08c01"],
+      ["Questions_In_Review", "8889577c"],
+      ["QA", "b58f4901"],
+      ["UAT", "18b48c1f"],
+      ["QA_Senior_Review", "4bf4d791"],
+      ["Done", "199de7b8"],
+      ["Development_Team", "79b0f439"],
+      ["PR_Needs_Review",  "d3dec1c7"],
+      ["PR_Pending_Approval", "a113bef6"],
+      ["PR_Approved", "21e33c3c"],
+      ["Feature_Branch", "024a83ed"],
+      ["IGNORE", "d662f195"],
     ]);
 
     statusId = statusValues.get(statusField);
+    console.log(`statusId: ${statusId}`):
     return statusId;
 }
 
