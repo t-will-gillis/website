159e47900eeaff5ef7bcf9e40de236b52891dc97
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-21 11:08:57 -0700

    Update post-labels-comment.js

--- github-actions/trigger-issue/add-missing-labels-to-issues/post-labels-comment.js
+++ github-actions/trigger-issue/add-missing-labels-to-issues/post-labels-comment.js
@@ -1,6 +1,6 @@
-var fs = require("fs")
-const postComment = require('../../utils/post-issue-comment')
-const formatComment = require('../../utils/format-comment')
+var fs = require("fs");
+const postComment = require('../../utils/post-issue-comment');
+const formatComment = require('../../utils/format-comment');
 
 // Constant variables
 const LABELS_OBJ = {
@@ -8,11 +8,11 @@ const LABELS_OBJ = {
   'role missing': 'Role',
   'Feature Missing': 'Feature',
   'size: missing': 'Size'
-}
+};
 
 // Global variables
-var github
-var context
+var github;
+var context;
 
 /**
  * Format the label instructions into a template, then post it to the issue
@@ -23,24 +23,26 @@ var context
  * @param {Number} issueNum - the number of the issue where the post will be made 
  */
 async function main({ g, c }, { actionResult, addedLabels, issueNum }) {
-  github = g
-  context = c
-
+  github = g;
+  context = c;
+  
+  const issueTitle = context.payload.issue.title
+  
   // If the previous action failed, stop here
-  if (actionResult === false){
+  if (actionResult === false) {
     console.log('Previous gh-action failed. The current gh-action will end.');
-    return
+    return;
   }
 
   // If addedLabels === [], no new labels are needed
-  if (addedLabels.length === 0) {
+  if (addedLabels.length === 0 || issueTitle.includes('Hack for LA website bot')) {
     console.log('All required labels have been included; the labels comment will not be posted.');
-    return
+    return;
   }
 
   console.log('Comment will be posted to issue regarding missing labels.');
-  const instructions = makeComment(addedLabels)
-  await postComment(issueNum, instructions, github, context)
+  const instructions = makeComment(addedLabels);
+  await postComment(issueNum, instructions, github, context);
 }
 
 /**
@@ -49,7 +51,7 @@ async function main({ g, c }, { actionResult, addedLabels, issueNum }) {
  * @return {String} - returns a string of instructions to be used for the comment
  */
 function makeComment(labels) {
-  const issueCreator = context.payload.issue.user.login
+  const issueCreator = context.payload.issue.user.login;
 
   if (labels.length === 0) {
     return;
