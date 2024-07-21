0039d16305f3a20887dee6e6e3d677ec52a10df1
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-21 09:29:13 -0700

    Update get-issue-id.js

--- github-actions/utils/get-issue-id.js
+++ github-actions/utils/get-issue-id.js
@@ -1,8 +1,23 @@
+/**
+ * Function to return node id for issue
+ * @param {Object} github     - github object from actions/github-script
+ * @param {Object} context    - context object from actions/github-script
+ * @param {Int} issueNum      - the issue number
+ * @returns {String} issueId  - the issue's node id for use in GraphQL
+ */
+async function getIssueId(github, context, issueNum) {
 
+  const { owner, repo } = github.context
 
+  // Ref: https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#get-an-issue
+  const issueResults = await github.request('GET /repos/{owner}/{repo}/issues/{issue_number}', {
+    owner: owner,
+    repo: repo,
+    issue_number: issueNum
+  });
 
-function getIssueId(issueNum) {
-
-  await
-  
+  const issueId = issueResults.data.node_id;
+  return issueId;  
 }
+
+module.exports = getIssueId;
