8daa88ef493aa307f634c1b8b66f971a76d51b09
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-21 10:47:08 -0700

    Update query-issue-info.js

--- github-actions/utils/query-issue-info.js
+++ github-actions/utils/query-issue-info.js
@@ -36,14 +36,14 @@ async function queryIssueInfo(github, context, issueNum) {
 
   try { 
     const response = await github.graphql(query, variables);
-    console.log(response)
+
     // Extract the list of project items associated with the issue
     const projectItems = response.repository.issue.projectItems.nodes;
-    console.log(projectItems);
+
     // Since there is always one item associated with the issue,
     // directly get the item's ID from the first index
     const id = projectItems[0].id;
-    console.log(id);
+
     // Iterate through the field values of the first project item
     // and find the node that contains the 'name' property, then get its 'name' value
     const statusName = projectItems[0].fieldValues.nodes.find(item => item.hasOwnProperty('name')).name;
