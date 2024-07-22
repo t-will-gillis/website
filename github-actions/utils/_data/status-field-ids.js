724cc885043735ee15ce8a2b9102b8c29c9ba36e
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-21 20:17:41 -0700

    Update status-field-ids.js

--- github-actions/utils/_data/status-field-ids.js
+++ github-actions/utils/_data/status-field-ids.js
@@ -9,27 +9,49 @@
 
 function statusFieldIds(statusField) {
 
-  const statusValues = new Map([
-    ["Agendas", "864392c1"],
-    ["Ice_Box", "2b49cbab"],
-    ["Emergent_Requests", "d468e876"],    
-    ["New_Issue_Approval", "83187325"],
-    ["Prioritized_Backlog", "434304a8"],
-    ["ERs_And_Epics", "c81aac49"],
-    ["In_Progress", "9a878e9c"],
-    // ["Questions_In_Review", "53b56f8d"],
+  // const statusValues = new Map([
+  //   ["Agendas", "864392c1"],
+  //   ["Ice_Box", "2b49cbab"],
+  //   ["Emergent_Requests", "d468e876"],    
+  //   ["New_Issue_Approval", "83187325"],
+  //   ["Prioritized_Backlog", "434304a8"],
+  //   ["ERs_And_Epics", "c81aac49"],
+  //   ["In_Progress", "9a878e9c"],
+  //   // ["Questions_In_Review", "53b56f8d"],
+  //   ["Questions_In_Review", "8889577c"],
+  //   ["QA", "d013db69"],
+  //   ["UAT", "8fa184b4"],
+  //   ["QA_Senior_Review", "dcf54222"],
+  //   ["Done", "f8ad2dcb"],
+  //   ["Development_Team", "68334e89"],
+  //   ["PR_Needs_Review", "e6140194"],
+  //   ["PR_Pending_Approval", "b8ef8e27"],
+  //   ["PR_Approved", "70e431d8"],
+  //   ["Feature_Branch", "88d8c3d6"],
+  //   ["IGNORE", "0728afac"],
+  // ]);
+
+  const statusValues = new Map([          // for TWG
+    ["Agendas", "941e9965"],
+    ["Ice_Box", "d039008d"],
+    ["Emergent_Requests", "8749a929"],    
+    ["New_Issue_Approval", "31082c42"],
+    ["Prioritized_Backlog", "3d615590"],
+    ["ERs_And_Epics", "387a9d00"],
+    ["In_Progress", "4be08c01"],
     ["Questions_In_Review", "8889577c"],
-    ["QA", "d013db69"],
-    ["UAT", "8fa184b4"],
-    ["QA_Senior_Review", "dcf54222"],
-    ["Done", "f8ad2dcb"],
-    ["Development_Team", "68334e89"],
-    ["PR_Needs_Review", "e6140194"],
-    ["PR_Pending_Approval", "b8ef8e27"],
-    ["PR_Approved", "70e431d8"],
-    ["Feature_Branch", "88d8c3d6"],
-    ["IGNORE", "0728afac"],
+    ["QA", "b58f4901"],
+    ["UAT", "18b48c1f"],
+    ["QA_Senior_Review", "4bf4d791"],
+    ["Done", "199de7b8"],
+    ["Development_Team", "79b0f439"],
+    ["PR_Needs_Review",  "d3dec1c7"],
+    ["PR_Pending_Approval", "a113bef6"],
+    ["PR_Approved", "21e33c3c"],
+    ["Feature_Branch", "024a83ed"],
+    ["IGNORE", "d662f195"],
   ]);
+
 
   statusId = statusValues.get(statusField);
 
