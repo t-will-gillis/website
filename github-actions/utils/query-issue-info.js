300b8dce040d821c39d92ed36dc159bc606fd000
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-07-21 20:35:11 -0700

    Update query-issue-info.js

--- github-actions/utils/query-issue-info.js
+++ github-actions/utils/query-issue-info.js
@@ -37,15 +37,15 @@ async function queryIssueInfo(github, context, issueNum) {
 
     // Extract the list of project items associated with the issue
     const projectItems = response.repository.issue.projectItems.nodes;
-
+    console.log(projectItems);
     // Since there is always one item associated with the issue,
     // directly get the item's ID from the first index
     const id = projectItems[0].id;
-
+    console.log(id);
     // Iterate through the field values of the first project item
     // and find the node that contains the 'name' property, then get its 'name' value
     const statusName = projectItems[0].fieldValues.nodes.find(item => item.hasOwnProperty('name')).name;
-
+    console.log(statusName);
     return { id, statusName };
   } catch(error) {
     throw new Error(`Error finding Issue #${issueNum} id and status; error = ${error}`);
