78ddfef9c07f65197ec639bb96aadfe5de6bf1ac
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-08-02 14:57:26 -0700

    Update check-team-membership.js

--- github-actions/utils/check-team-membership.js
+++ github-actions/utils/check-team-membership.js
@@ -2,7 +2,6 @@
 * @param {octokit} github - Octokit object used to access GitHub API 
 * @param {String} githubUsername - The GitHub username of the user whose membership is to be checked.
 * @param {String} team - The HFLA team the username's membership is checked against. Example: 'website-write'
-
 - Returns true or false depending on whether the username is found on the passed team, 404 means the user passed wasn't 
 found on the team passed. Any other type of error will be thrown.
 - Need read:org permission to use this function, the least permissive token which contains this is the TEAMS token.
@@ -15,7 +14,7 @@ async function isMemberOfTeam(github, githubUsername, team)
 {
     try {
         await github.rest.teams.getMembershipForUserInOrg({
-            org : 'hackforla',    // replace with context.repo.org 
+            org : 'hackforla',
             team_slug : team,
             username : githubUsername
         });
