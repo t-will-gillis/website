bbdbea50eed54d83477f6a77be37ef3558107e61
t-will-gillis-dummy <121192871+t-will-gillis-dummy@users.noreply.github.com>
2024-08-01 19:51:03 -0700

    Update verify-pr.js

--- github-actions/trigger-pr-target/verify-pr.js
+++ github-actions/trigger-pr-target/verify-pr.js
@@ -1,11 +1,10 @@
 const isMemberOfTeam = require('../utils/check-team-membership');
 const commentContent = 'You must be a member of the HFLA website team in order to create pull requests. Please see our page on how to join us as a member at HFLA: https://www.hackforla.org/getting-started.  If you have been though onboarding, and feel this message was sent in error, please message us in the #hfla-site team Slack channel with the link to this PR.';
-
-async function main({github,context}) {
+// junk
+async function main({ github,context }) {
     const prAuthor = context.payload.sender.login;
     const prNumber = context.payload.number;
     const isMember = await isMemberOfTeam(github, prAuthor, 'website-write');
-    console.log(`'${prAuthor}' is a member of 'website-write' ${isMember}`); 
     if (isMember) {    
         console.log('Successfully verified!');
     }
