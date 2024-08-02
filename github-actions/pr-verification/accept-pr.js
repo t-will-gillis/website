84efccfaafec2af52c360dfcfe59336bdd7c384f
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-08-02 14:04:10 -0700

    Update accept-pr.js

--- github-actions/pr-verification/accept-pr.js
+++ github-actions/pr-verification/accept-pr.js
@@ -19,7 +19,7 @@ async function main({ g, c }) {
   if (!team_check) {
     console.log(`${prAuthor} is not a member of ${team}- closing PR with comment`);
     let comment = 'You must be a member of the HFLA website team in order to create pull requests. Please see our page on how to join us as a member at HFLA: https://www.hackforla.org/getting-started. If you have been though onboarding, and feel you have received this message in error, please message us in the #hfla-site team Slack channel with the link to this PR.';
-    postIssueComment(prNumber, comment, github, context);
+    await postIssueComment(prNumber, comment, github, context);
     await github.rest.pulls.update({
         owner : 't-will-gillis',
         // owner: context.repo.owner,
