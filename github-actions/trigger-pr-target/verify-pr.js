945e1d6aefd2316a10da9d4ff252b32b161f0429
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-08-02 14:56:21 -0700

    Update verify-pr.js
    original with console.log(), switch to self repo

--- github-actions/trigger-pr-target/verify-pr.js
+++ github-actions/trigger-pr-target/verify-pr.js
@@ -1,7 +1,7 @@
 const isMemberOfTeam = require('../utils/check-team-membership');
 const commentContent = 'You must be a member of the HFLA website team in order to create pull requests. Please see our page on how to join us as a member at HFLA: https://www.hackforla.org/getting-started.  If you have been though onboarding, and feel this message was sent in error, please message us in the #hfla-site team Slack channel with the link to this PR.';
 
-async function main({ github,context }) {
+async function main({github,context}) {
     const prAuthor = context.payload.sender.login;
     const prNumber = context.payload.number;
     const isMember = await isMemberOfTeam(github, prAuthor, 'website-write');
@@ -12,13 +12,15 @@ async function main({ github,context }) {
         console.log(`'${prAuthor}' not a member; closing`);
         try {
             await github.rest.issues.update({
-                owner : 't-will-gillis',    // twg edits
+                // owner : 'hackforla',
+                owner: 't-will-gillis',
                 repo : 'website',
                 issue_number : prNumber,
                 state : 'closed'
             });
             await github.rest.issues.createComment({
-                owner : 't-will-gillis',     // twg edits
+                // owner : 'hackforla',
+                owner: 't-will-gillis',
                 repo : 'website',
                 issue_number : prNumber,
                 body : commentContent
