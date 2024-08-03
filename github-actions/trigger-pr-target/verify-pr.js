9b45db8f44f41fa2394d1d2dbd1389b73c900e8e
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-08-03 14:14:59 -0700

    Update verify-pr.js

--- github-actions/trigger-pr-target/verify-pr.js
+++ github-actions/trigger-pr-target/verify-pr.js
@@ -1,5 +1,6 @@
 const isMemberOfTeam = require('../utils/check-team-membership');
 const commentContent = 'You must be a member of the HFLA website team in order to create pull requests. Please see our page on how to join us as a member at HFLA: https://www.hackforla.org/getting-started.  If you have been though onboarding, and feel this message was sent in error, please message us in the #hfla-site team Slack channel with the link to this PR.';
+const commentContent2 = 'You are verified!';
 
 async function main({github,context}) {
     const prAuthor = context.payload.sender.login;
@@ -7,6 +8,13 @@ async function main({github,context}) {
     const isMember = await isMemberOfTeam(github, prAuthor, 'website-write');
     if (isMember) {    
         console.log('Successfully verified!');
+        await github.rest.issues.createComment({
+                // owner : 'hackforla',
+                owner: 't-will-gillis',
+                repo : 'website',
+                issue_number : prNumber,
+                body : commentContent2
+            });
     }
     else {
         console.log(`'${prAuthor}' not a member; closing`);
