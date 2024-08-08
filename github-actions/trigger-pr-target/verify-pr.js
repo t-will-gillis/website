734354ce465efa01beedfb866dc513fe48cabad2
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-08-07 19:54:58 -0700

    Update verify-pr.js

--- github-actions/trigger-pr-target/verify-pr.js
+++ github-actions/trigger-pr-target/verify-pr.js
@@ -1,39 +1,28 @@
-/** 
-* @param {octokit} github - Octokit object used to access GitHub API 
-* @param {String} githubUsername - The GitHub username of the user whose membership is to be checked.
-* @param {String} team - The HFLA team the username's membership is checked against. Example: 'website-write'
-- Returns true or false depending on whether the username is found on the passed team, 404 means the user passed wasn't 
-found on the team passed. Any other type of error will be thrown.
-- Need read:org permission to use this function, the least permissive token which contains this is the TEAMS token.
-Lack of permission will result in a 403 error.
-- The method of obtaining the GitHub username will vary depending on the contents of the context object. See GitHub action 
-docs on printing context information into the log.
-*/
 
-async function isMemberOfTeam(github, context) {
+const commentContent = 'You must be a member of the HFLA website team in order to create pull requests. Please see our page on how to join us as a member at HFLA: https://www.hackforla.org/getting-started. If you have been though onboarding and feel this message was sent in error, please message us in the #hfla-site team Slack channel with the link to this PR.';
 
-    // const org = ;
-    const username = 't-will-gillis';
-    const team = 'website-write';
-    // console.log(context.repo);
+async function main({ github,context }) {
+    const prAuthor = context.payload.sender.login;
+    const prNumber = context.payload.number;
     
+ 
     try {
-        await github.rest.teams.getMembershipForUserInOrg({
-            org: 'hackforla',
-            team_slug: team,
-            username: username
+        await github.rest.issues.update({
+            owner : 'hackforla',
+            repo : 'website',
+            issue_number : prNumber,
+            state : 'closed'
         });
-        console.log(`Found '${username}' on team`);
-        return true;
-    } catch (verificationError) {
-        console.log(`Status: ${verificationError.status}- '${username}' not found on team`);
-        if (verificationError.status == 404) {
-            return false;
-        }
-        else {
-            throw verificationError;
-        }
-    }
+        await github.rest.issues.createComment({
+            owner : 'hackforla',
+            repo : 'website',
+            issue_number : prNumber,
+            body : commentContent
+        }); 
+    } catch (closeCommentError) {
+        throw closeCommentError;
+    } 
+        
 }
 
-module.exports = isMemberOfTeam;
+module.exports = main;
