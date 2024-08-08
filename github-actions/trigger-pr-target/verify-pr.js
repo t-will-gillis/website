8120aa841f0e4a02267149d5313a4ae76c28003d
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-08-07 20:09:59 -0700

    Update verify-pr.js

--- github-actions/trigger-pr-target/verify-pr.js
+++ github-actions/trigger-pr-target/verify-pr.js
@@ -1,28 +1,30 @@
 
-const commentContent = 'You must be a member of the HFLA website team in order to create pull requests. Please see our page on how to join us as a member at HFLA: https://www.hackforla.org/getting-started. If you have been though onboarding and feel this message was sent in error, please message us in the #hfla-site team Slack channel with the link to this PR.';
 
-async function main({ github,context }) {
-    const prAuthor = context.payload.sender.login;
-    const prNumber = context.payload.number;
-    
- 
-    try {
-        await github.rest.issues.update({
-            owner : 'hackforla',
-            repo : 'website',
-            issue_number : prNumber,
-            state : 'closed'
-        });
-        await github.rest.issues.createComment({
-            owner : 'hackforla',
-            repo : 'website',
-            issue_number : prNumber,
-            body : commentContent
-        }); 
-    } catch (closeCommentError) {
-        throw closeCommentError;
-    } 
-        
+async function main({ github, context }) {
+
+  const prNumber = context.payload.number;
+  const commentContent =
+    "You must be a member of the HFLA website team in order to create pull requests. Please see our page \
+      on how to join us as a member at HFLA: https://www.hackforla.org/getting-started. If you have been \
+      though onboarding and feel this message was sent in error, please message us in the #hfla-site team \
+      Slack channel with the link to this PR.";
+
+  try {
+    await github.rest.issues.update({
+      owner: "t-will-gillis",
+      repo: context.payload.repo,
+      issue_number: prNumber,
+      state: "closed",
+    });
+    await github.rest.issues.createComment({
+      owner: "t-will-gillis",
+      repo: context.payload.repo,
+      issue_number: prNumber,
+      body: commentContent,
+    });
+  } catch (closeCommentError) {
+    throw closeCommentError;
+  }
 }
 
 module.exports = main;
