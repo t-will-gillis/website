634c24cbf4ead713f5700d96a363bcd0d323b114
t-will-gillis-dummy <121192871+t-will-gillis-dummy@users.noreply.github.com>
2024-09-09 10:30:22 -0700

    Update preliminary-update-comment.js

--- github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
+++ github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
@@ -1,10 +1,10 @@
-// Import modules
+// Import modules 
 const fs = require("fs");
 const postComment = require('../../utils/post-issue-comment');
 const formatComment = require('../../utils/format-comment');
 const getTimeline = require('../../utils/get-timeline');
 const getTeamMembers = require('../../utils/get-team-members');
-
+// junk comment
 // Global variables
 let github;
 let context;
