356c835e3cdd947c5c0346e205afe0f29dfe2d45
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2024-10-03 16:04:07 -0700

    Update retrieve-label-directory.js

--- github-actions/utils/retrieve-label-directory.js
+++ github-actions/utils/retrieve-label-directory.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 // Global variables
 var filepath = 'github-actions/utils/_data/label-directory.json';
+var labelData;
 
 /*
  * Matches label reference name(s) to the label display name(s) from JSON
@@ -13,22 +14,27 @@ function labelRetrieveNames(...labelKeys) {
 
   // Retrieve label directory
   const rawData = fs.readFileSync(filepath, 'utf8');
-  const labelData = JSON.parse(rawData);
+  if (labelData === undefined) {
+    console.log(`creating Label Data`);
+    const rawData = fs.readFileSync(filepath, 'utf8');
+    labelData = JSON.parse(rawData);
+  } else {
+    console.log(`data exists`);
+  }
+  
+  let labelKey = '';
+  let labelName = ''
 
-  const labelId = labelKeys[0];
-  console.log(labelId)
-  const labelName = labelData[labelId];
-  console.log(labelName);
   try {
-    displayNames = labelData[labelKeys[0]];
-    console.log(displayNames)
-    console.log(`Success! Found labelKey: '${labelKeys[0]}', returning labelName: '${labelData[labelKeys[0]]}'`);
+    labelKey = labelKeys[0];
+    labelName = labelData[labelKey][0];
+    console.log(`Success! Found labelKey: '${labelKey}', returning labelName: '${labelName}'`);
   } catch (err) {
-    console.error(`Failed to find labelKey: '${labelKeys[0]}'`)
+    console.error(`Failed to find labelKey: '${labelKey}'`)
   }
 
 
-  return displayNames;
+  return labelName;
 
 }
 
@@ -41,7 +47,7 @@ module.exports = labelRetrieveNames;
 
 // // Global variables
 // var filepath = 'github-actions/utils/_data/label-directory.json';
-// var labelData;
+
 // /*
 //  * Matches label reference name(s) to the label display name(s) from JSON
 //  * @param {string } filepath     - Path to `label_directory.json`
