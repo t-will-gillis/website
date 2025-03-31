167f64e119e1155bf8af60ad0aa31bd7ce5aff85
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-03-31 13:44:05 -0700

    Update check-team-membership.js

--- github-actions/utils/check-team-membership.js
+++ github-actions/utils/check-team-membership.js
@@ -19,9 +19,11 @@ async function isMemberOfTeam(github, githubUsername, team)
             team_slug : team,
             username : githubUsername
         });
+        console.log(`User ${githubusername} is member of team ${team}`);
         return true;
     } catch (verificationError) {
         if (verificationError.status == 404) {
+            console.log(`User ${githubusername} is not a team member`);
             return false;
         }
         else {
