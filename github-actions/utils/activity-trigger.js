dafca341a3b7dcfc8357258c3722b82544d85f56
t-will-gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-07-15 15:55:53 -0700

    n

--- github-actions/utils/activity-trigger.js
+++ github-actions/utils/activity-trigger.js
@@ -7,22 +7,12 @@ async function activityTrigger({g, c}, eventName, eventAction, eventActor) {
     github = g;
     context = c;
 
-    // const actEventS = JSON.stringify(actEvent, null, 2); 
-    // const act_action = github.event.action; 
-    // const act_activity = github.event_name;
-    const actor = github.actor;
     const issueNum = context.payload.issue.number;
-    // const issueContext = context.payload.issue;
-    // const contextT = JSON.stringify(issueContext);
 
     console.log(`eventName = ${eventName}`);
     console.log(`eventAction = ${eventAction}`);
     console.log(`eventActor = ${eventActor}`);
-    console.log(`actor = ${actor}`);
     console.log(`issueNum = ${issueNum}`);
-    console.log('------------------------------');
-
-    // console.log(`context = ${contextT}`);
 }
 
 module.exports = activityTrigger;
