08427e6855278002c682ee18c2ecff5c57bf536f
t-will-gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-07-15 14:36:36 -0700

    whittle down

--- github-actions/utils/activity-trigger.js
+++ github-actions/utils/activity-trigger.js
@@ -6,12 +6,12 @@ async function activityTrigger(github, context) {
 
     const act_event = github.event; 
     // const act_action = github.event.action; 
-    const issueNum = context.payload.issue.number;
+    // const issueNum = context.payload.issue.number;
 
 
     console.log(`act_event = ${act_event}`);
     // console.log(`act_action = ${act_action}`);
-    console.log(`issueNum = ${issueNum}`);
+    // console.log(`issueNum = ${issueNum}`);
     console.log('------------------------------');
     console.log(`context = ${context}`);
 }
