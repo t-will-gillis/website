b95ddc43c43f2ca042183c9fe69db809454d1cac
t-will-gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-07-15 16:10:39 -0700

    finalize

--- github-actions/utils/activity-trigger.js
+++ github-actions/utils/activity-trigger.js
@@ -6,8 +6,15 @@ async function activityTrigger({g, c}, eventName, eventAction, eventActor) {
 
     github = g;
     context = c;
+    let issueNum = '';
 
-    const issueNum = context.payload.issue.number;
+    if (eventName.includes('issue') {
+        issueNum = context.payload.issue.number;
+    } else if (eventName.includes('pull_request') {
+        issueNum = context.payload.pull_request.number;
+    } else {
+        issueNum = 'unknown'
+    }
 
     console.log(`eventName = ${eventName}`);
     console.log(`eventAction = ${eventAction}`);
