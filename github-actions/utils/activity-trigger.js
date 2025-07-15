5d7f29c7cd05437d7552b515f05a34499ed64122
t-will-gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-07-15 14:48:35 -0700

    addl

--- github-actions/utils/activity-trigger.js
+++ github-actions/utils/activity-trigger.js
@@ -2,16 +2,16 @@
 
 
 
-async function activityTrigger(github, context) {
+async function activityTrigger({github, context}, actEvent, actAction) {
 
     // const act_event = github.event; 
     // const act_action = github.event.action; 
     // const issueNum = context.payload.issue.number;
-    const issueContext = context.payload.issue;
+    // const issueContext = context.payload.issue;
 
-    // console.log(`act_event = ${act_event}`);
-    // console.log(`act_action = ${act_action}`);
-    console.log(`issueContext = ${issueContext}`);
+    console.log(`actEvent = ${actEvent}`);
+    console.log(`actAction = ${actAction}`);
+    // console.log(`issueNum = ${issueNum}`);
     console.log('------------------------------');
     console.log(`context = ${github.context}`);
 }
