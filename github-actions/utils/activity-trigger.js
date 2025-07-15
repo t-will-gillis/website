7c085a422d144f31340080eacb44e46aeabbdb75
t-will-gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-07-15 14:42:04 -0700

    blech

--- github-actions/utils/activity-trigger.js
+++ github-actions/utils/activity-trigger.js
@@ -4,16 +4,16 @@
 
 async function activityTrigger(github, context) {
 
-    const act_event = github.event; 
+    // const act_event = github.event; 
     // const act_action = github.event.action; 
     // const issueNum = context.payload.issue.number;
+    const issueContext = context.payload.issue;
 
-
-    console.log(`act_event = ${act_event}`);
+    // console.log(`act_event = ${act_event}`);
     // console.log(`act_action = ${act_action}`);
-    // console.log(`issueNum = ${issueNum}`);
+    console.log(`issueContext = ${issueContext}`);
     console.log('------------------------------');
-    console.log(`context = ${context}`);
+    console.log(`context = ${github.context}`);
 }
 
 module.exports = activityTrigger;
