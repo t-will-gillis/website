b593fb582a077fb84985ad6feddcd7177032ea45
t-will-gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-07-15 18:17:11 -0700

    p

--- github-actions/utils/activity-trigger.js
+++ github-actions/utils/activity-trigger.js
@@ -6,20 +6,30 @@ async function activityTrigger({g, c}, eventName, eventAction, eventActor) {
 
     github = g;
     context = c;
+
     let issueNum = '';
-
-    if (eventName.includes('issue')) {
-        issueNum = context.payload.issue.number;
-    } else if (eventName.includes('pull_request')) {
-        issueNum = context.payload.pull_request.number;
-    } else {
-        issueNum = 'unknown'
-    }
-
+    let assignee = '';
+    
     console.log(`eventName = ${eventName}`);
     console.log(`eventAction = ${eventAction}`);
     console.log(`eventActor = ${eventActor}`);
     console.log(`issueNum = ${issueNum}`);
+
+    if (eventName.includes('issue')) {
+        issueNum = context.payload.issue.number;
+        assignee = context.payload.issue.assignee;
+        console.log(context.payload.issue.assignee);
+        if (eventAction.includes('assigned')) {
+            eventActor = assignee;
+        }
+    } else if (eventName.includes('pull_request')) {
+        issueNum = context.payload.pull_request.number;
+        console.log(`author = ${context.payload.pull_request.author}`)
+    } else {
+        issueNum = 'unknown'
+    }
+
+
 }
 
 module.exports = activityTrigger;
