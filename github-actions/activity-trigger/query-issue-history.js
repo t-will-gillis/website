6d598155fdf47491ebbf84fc7350feb9cb9254f2
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-07-28 22:01:42 -0700

    Update query-issue-history.js

--- github-actions/activity-trigger/query-issue-history.js
+++ github-actions/activity-trigger/query-issue-history.js
@@ -145,7 +145,7 @@ async function queryIssueHistory({g, c}) {
   
       // Iterate through the timeline field values to extract actors, events, timelines
       timelineItems.filter(item => relevantTypes.has(item.__typename)).map(item => {    
-        const { __typename, createdAt } = item;
+        const { __typename, url, createdAt } = item;
     
         let actor = '';
         let reason = '';
@@ -156,7 +156,7 @@ async function queryIssueHistory({g, c}) {
         } else if (__typename === 'ClosedEvent') {
           actor = item.actor.login;
           prUrl = item.url;
-          console.log(item.url);
+          console.log(item);
           reason = item.stateReason;
           prEvent = 'PR_'+ reason;
         }
