f984115ee60ff68d6e6e7235e16dc07f23e26303
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-08-04 22:06:55 -0700

    Update query-issue-history.js

--- github-actions/activity-trigger/query-issue-history.js
+++ github-actions/activity-trigger/query-issue-history.js
@@ -13,8 +13,8 @@ async function queryIssueHistory({g, c}) {
   
   let history = [];
   
-  let start = 584;
-  let end = 584;
+  let start = 586;
+  let end = 586;
   for (let i = start; i <= end; i++) {
     let issueNum = i;
   
