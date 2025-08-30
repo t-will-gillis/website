f2a72d021d53599af63dc3198f43a2efa638e30c
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-08-29 20:54:30 -0700

    Update status-field-ids.js

--- github-actions/utils/_data/status-field-ids.js
+++ github-actions/utils/_data/status-field-ids.js
@@ -11,10 +11,13 @@ function statusFieldIds(statusField) {
 
   const statusValues = new Map([
 
-    // Default values for HfLA Website Project 86
-    ["PROJECT_ID", "PVT_kwDOALGKNs4Ajuck"],
-    ["FIELD_ID", "PVTSSF_lADOALGKNs4AjuckzgcCutQ"],
 
+    // Default values for HfLA Website Project 86
+    // ["PROJECT_ID", "PVT_kwDOALGKNs4Ajuck"],
+    // ["FIELD_ID", "PVTSSF_lADOALGKNs4AjuckzgcCutQ"],
+    ["PROJECT_ID", "PVT_kwHOAm6MB84Aiu3X"],          // wg
+    ["FIELD_ID", "PVTSSF_lAHOAm6MB84Aiu3XzgbNC9E"],  // wg
+   
     // Individual Status field values
     ["Agendas", "864392c1"],
     ["Ice_Box", "2b49cbab"],
@@ -22,7 +25,8 @@ function statusFieldIds(statusField) {
     ["New_Issue_Approval", "83187325"],
     ["Prioritized_Backlog", "434304a8"],
     ["ERs_And_Epics_Ready", "c81aac49"],
-    ["In_Progress", "9a878e9c"],
+    // ["In_Progress", "9a878e9c"],
+    ["In_Progress", "4be08c01"],  // wg
     ["Questions_In_Review", "53b56f8d"],
     ["QA", "d013db69"],
     ["UAT", "8fa184b4"],
@@ -35,7 +39,7 @@ function statusFieldIds(statusField) {
     ["Feature_Branch", "88d8c3d6"],
     ["IGNORE", "0728afac"],
   ]);
-
+ 
   const statusId = statusValues.get(statusField);
 
   return statusId;
