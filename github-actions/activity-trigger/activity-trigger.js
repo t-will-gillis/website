e8be0d69f2612298811ab28e7af0aec9a74d7650
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-11-29 10:16:49 -0800

    Update activity-trigger.js
    Comment added to test PRs with updated `actions/checkout@v6`

--- github-actions/activity-trigger/activity-trigger.js
+++ github-actions/activity-trigger/activity-trigger.js
@@ -1,3 +1,5 @@
+// comment for testing prs
+
 /**
  * This function parses the triggered event to determine the trigger eventName and eventAction
  * and from this information decide the eventActor (user who is credited for the event).
