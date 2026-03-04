f2900e9440588b810ffc41f1450a99f86c7d2c75
t-will-gillis <339.colorado.dev@gmail.com>
2026-03-03 18:26:37 -0800

    trying to open pr triggers

--- github-actions/utils/check-team-membership.js
+++ github-actions/utils/check-team-membership.js
@@ -9,7 +9,7 @@
 async function isMemberOfTeam(github, context, username, team) {
   try {
     await github.rest.teams.getMembershipForUserInOrg({
-      org: context.repo.owner,
+      org: 'hackforla',
       team_slug: team,
       username: username,
     });
