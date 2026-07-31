ee536a9c7e6fd385293597965be5e24be140be01
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2026-07-30 19:57:02 -0700

    Add comment to check-linked-issue.js
    Added a comment to clarify the purpose of the code.

--- github-actions/trigger-pr/check-linked-issue.js
+++ github-actions/trigger-pr/check-linked-issue.js
@@ -4,7 +4,7 @@ const postIssueComment = require('../utils/post-issue-comment');
 // Global variables
 var github;
 var context;
-
+// more bs
 async function main({ g, c }) {
   github = g;
   context = c;
